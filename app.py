02c69839211d277ad84af34173606c358e606313
Emanuel Eirich <shanks-@gmx.de>
2025-06-22 20:57:52 +0200

    add mongo

--- app.py
+++ app.py
@@ -1,44 +1,49 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
 from pathlib import Path
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+
 import pandas as pd
+load_dotenv()  # take environment variables
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["*"])  # Enable CORS for all origins
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client[os.getenv("MONGO_DB_NAME")] # type: ignore
+duties = db.duties
 
-@app.route('/api/v1/fs', methods=['GET', 'POST'])
+@app.route('/api/v1/fs', methods=['GET', 'PATCH'])
 def home():
     print(request.method)
     if request.method == 'GET':
-        file = request.args.get('file')
-        if not file:
-            return jsonify(statuscode=400, error="File name is required")
-        file += ".json"
-        cwd = Path.cwd()
-        file_path = Path(cwd, "data", file)
-        if not file_path.exists():
-            return jsonify(statuscode=404, error="File not found")
-        df = pd.read_json(file_path)
-        json_data = df.to_dict(orient="records")
-        if not json_data:
-            # If the file is empty, return an empty list
-            return jsonify(statuscode=200, data=[])
-        return jsonify(statuscode=200, data=json_data)
+        try:
+            mongo_data = duties.find()
+            filtered_data = []
+            for item in mongo_data:
+                dict_item = {
+                    "id": item["id"],
+                    "duty": item["duty"],
+                }
+                filtered_data.append(dict_item)
+            return jsonify(statuscode=200, data=filtered_data)
+        except Exception as e:
+            print(f"Error fetching data: {e}")
+            return jsonify(statuscode=500, message="Internal Server Error"), 500
     else:
-        data = request.get_json()
-        if not data:
-            return jsonify({"error": "No data provided"}), 400
-        file, json_data = data.values()
-
-        cwd = Path.cwd()
-        file_path = Path(cwd, "data")
-        if not file_path.exists():
-            Path.mkdir(file_path)
-        df = pd.DataFrame(json_data)
-        json_file = Path(file_path, file + ".json")
-        df.to_json(json_file, orient="records", indent=2)
-
-        return jsonify(statuscode=200, message="Data saved successfully")
+        try:
+            data = request.get_json()
+            if not data:
+                return jsonify({"error": "No data provided"}), 400
+            duties.find_one_and_update(
+                {"id": data["id"]},
+                {"$set": {"duty": data["duty"]}},
+            )
+            return jsonify(statuscode=200, message="Data saved successfully")
+        except Exception as e:
+            print(f"Error saving data: {e}")
+            return jsonify(statuscode=500, message="Internal Server Error"), 500
 
 
 if __name__ == '__main__':
